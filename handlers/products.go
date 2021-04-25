8527b3bf9c1decac1446ece889bc8fd979abf466
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2021-04-25 22:31:18 +0200

    Middleware to validate JSON while parsing using gorilla mux

--- handlers/products.go
+++ handlers/products.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"strconv"
@@ -12,6 +13,8 @@ import (
 type Products struct {
 	l *log.Logger
 }
+
+type KeyProduct struct{}
 
 func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
@@ -31,11 +34,7 @@ func (p *Products) GetProducts(rw http.ResponseWriter, r *http.Request) {
 func (p *Products) AddProduct(rw http.ResponseWriter, r *http.Request) {
 	p.l.Printf("Handle POST Product")
 
-	prod := &data.Product{}
-	err := prod.FromJSON(r.Body)
-	if err != nil {
-		http.Error(rw, "Unable to unmarshall JSON.", http.StatusBadRequest)
-	}
+	prod := r.Context().Value(KeyProduct{}).(*data.Product)
 
 	data.AddProduct(prod)
 }
@@ -49,12 +48,7 @@ func (p *Products) UpdateProduct(rw http.ResponseWriter, r *http.Request) {
 	}
 
 	p.l.Println("Handle PUT Product", id)
-
-	prod := &data.Product{}
-	err = prod.FromJSON(r.Body)
-	if err != nil {
-		http.Error(rw, "Unable to marshall JSON", http.StatusBadRequest)
-	}
+	prod := r.Context().Value(KeyProduct{}).(*data.Product)
 
 	err = data.UpdateProduct(id, prod)
 	if err == data.ErrProductNotFound {
@@ -68,18 +62,20 @@ func (p *Products) UpdateProduct(rw http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// type KeyProduct struct
+func (p Products) MiddlewareProductValidation(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		prod := &data.Product{}
+		err := prod.FromJSON(r.Body)
+		if err != nil {
+			p.l.Println("[ERROR] deserializing product", err)
+			http.Error(rw, "Unable to marshall JSON", http.StatusBadRequest)
+		}
 
-// func (p Products) MiddlewareProductValidation(next http.Handler) http.Handler {
+		// Add the product to the context
+		ctx := context.WithValue(r.Context(), KeyProduct{}, prod)
+		r = r.WithContext(ctx)
 
-// 	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
-// 		prod := &data.Product{}
-// 		err := prod.FromJSON(r.Body)
-// 		if err != nil {
-// 			http.Error(rw, "Unable to unmarshall JSON.", http.StatusBadRequest)
-// 			return
-// 		}
-
-// 		ctx := r.Context().WithValue()
-// 	})
-// }
+		// Call the next handler, which can be another middleware in the chain, or the final handler.
+		next.ServeHTTP(rw, r)
+	})
+}
